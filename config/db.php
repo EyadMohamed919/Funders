a4863ba88f24e92d105166ea18c41c8d523be9dc
Eyad Mahmoud <139317529+EyadMohamed919@users.noreply.github.com>
2026-03-19 15:12:45 +0200

    Initialized project

---
+++ config/db.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/..\vendor\autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else
+{
+    echo "Connected to MySQL Database";
+}
+
+function getDatabaseConnection()
+{
+    global $conn;
+    return $conn;
+}
+
+?>
