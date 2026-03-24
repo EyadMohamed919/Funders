6010e2d84ff36f3d0aaf13b58b1727f8323f030b
Eyad Mahmoud <139317529+EyadMohamed919@users.noreply.github.com>
2026-03-24 14:33:51 +0200

    Sanitized donation code

--- src/controller/DonationController.php
+++ src/controller/DonationController.php
@@ -39,47 +39,5 @@ class DonationController {
         }
     }
 
-    public function updateDonation() {
-    $_POST = json_decode(file_get_contents("php://input"), true);
 
-    if (empty($_POST['donation_id']) || empty($_POST['donation_amount']) || 
-        empty($_POST['donation_date'])  || empty($_POST['user_id'])) {
-        http_response_code(400);
-        echo json_encode(['error' => 'All fields are required']);
-        return;
-    }
-    
-    $success = $this->DonationModel->UpdateDonation(
-        $_POST['donation_id'],
-        $_POST['donation_amount'],
-        $_POST['donation_date'],
-        $_POST['user_id']
-    );
-    if ($success) {
-            http_response_code(201);
-            echo json_encode(['message' => 'Donation updated successfully']);
-        } else {
-            http_response_code(500);
-            echo json_encode(['error' => 'Failed to update donation']);
-        }
-    }
-
-    public function deleteDonation(){
-        $_POST= json_decode(file_get_contents('php://input'), true);
-        if (empty($_POST['donation_id'] || empty($_POST['user_id']))) {
-            http_response_code(400);
-            echo json_encode(['error' => 'donation_id and user_id are required']);
-            return;
-        }
-        $success = $this->DonationModel->deleteDonation(
-            $_POST['donation_id'], 
-            $_POST['user_id']);
-        if ($success) {
-            http_response_code(200);
-            echo json_encode(['message' => 'Donation deleted successfully']);
-        } else {
-            http_response_code(500);
-            echo json_encode(['error' => 'Failed to delete donation']);
-        }
-    }
 }
