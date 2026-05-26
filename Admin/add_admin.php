519aaf576b2cce505f9c2373e9e6ab73969cf66d
Eyad Mahmoud <139317529+EyadMohamed919@users.noreply.github.com>
2026-05-26 12:53:10 +0300

    PROJECT PHASE 2

--- Admin/add_admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "adminModel.php";
-$model = new Model();
-
-$model->addAdmin($_POST["username"], $_POST["password"]);
-
-header("Location: AdminMangement.php");
-?>
--- Admin/delete_admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "adminModel.php";
-$model = new Model();
-
-$model->deleteAdmin($_GET["username"]);
-
-header("Location: AdminMangement.php");
-?>
